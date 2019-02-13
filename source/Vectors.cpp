fb15e17949ead260fbed77e7e29d60025f6f12a5
Piotrek <pio.andr@gmail.com>
2019-02-13 14:05:23 +0100

    Fix tab to spaces

--- source/Vectors.cpp
+++ source/Vectors.cpp
@@ -8,38 +8,38 @@ using std::cout;
 unsigned int numOfCalls{0};
 
 struct object {
-	double x;
-	double y;
+    double x;
+    double y;
 
-	object(double x, double y)
-		: x(x), y(y) {}
+    object(double x, double y)
+        : x(x), y(y) {}
 
-	object(const object &source)
-		: x(source.x), y(source.y) {
-		numOfCalls++;
-	}
+    object(const object &source)
+        : x(source.x), y(source.y) {
+        numOfCalls++;
+    }
 };
 
 int main(int argc, char *argv[]) 
 {
-	Timer timer;
-	cout << "\nemplace_back with object passing\n";
-	timer.start();
-	std::vector<object> vec1{};
-	for(int x{0}; x<20; ++x) {
-		vec1.emplace_back(object{1, 1});
-	}
-	timer.stop();
-	cout << "Number of made copies:" << numOfCalls << "\n\n";
+    Timer timer;
+    cout << "\nemplace_back with object passing\n";
+    timer.start();
+    std::vector<object> vec1{};
+    for(int x{0}; x<20; ++x) {
+        vec1.emplace_back(object{1, 1});
+    }
+    timer.stop();
+    cout << "Number of made copies:" << numOfCalls << "\n\n";
 
-	numOfCalls=0;
+    numOfCalls=0;
 
-	cout << "emplace_back with parameters passing \n";
-	timer.start();
-	std::vector<object> vec2{};
-	for(int x{0}; x<20; ++x) {
-		vec2.emplace_back(1, 1);
-	}
-	timer.stop();
-	cout << "Number of made copies:" << numOfCalls << "\n\n";
+    cout << "emplace_back with parameters passing \n";
+    timer.start();
+    std::vector<object> vec2{};
+    for(int x{0}; x<20; ++x) {
+        vec2.emplace_back(1, 1);
+    }
+    timer.stop();
+    cout << "Number of made copies:" << numOfCalls << "\n\n";
 }
